c21c61fc543e079bd2d9bfb7c297ca7a751fd340
Biggie100 <140563155+Biggie100@users.noreply.github.com>
2025-03-28 14:53:10 +0000

    Add Sanskriti/app/src/main/java/com/example/sanskriti/SanskritiApplication.kt 1743173592612

--- Sanskriti/app/src/main/java/com/example/sanskriti/SanskritiApplication.kt
+++ Sanskriti/app/src/main/java/com/example/sanskriti/SanskritiApplication.kt
@@ -1,12 +1,8 @@
 package com.example.sanskriti
 
 import android.app.Application
-import com.google.firebase.FirebaseApp
-
 class SanskritiApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        // Initialize Firebase
-        FirebaseApp.initializeApp(this)
     }
 }
